7aaa888f585df6a3fdd5b4a3d2e732028cb428ed
Bale Tang <bale0331@foxmail.com>
2018-01-27 15:01:10 +0800

    people - deleted

--- src/states/People.js
+++ src/states/People.js
@@ -1,5 +1,6 @@
 export default class OurHuman {
 	constructor(row,col,low,midlow,midhigh,high,type,status,id) {
+
 		this.row = row;
 		this.col = col;
 		this.low = low;
@@ -15,70 +16,77 @@ export default class OurHuman {
 
 	}
 
-	create(){
-
-	}
-
-    update(){
-        
-    }
-
 	
 	timer() {
 		// TODO call timer() on each tile, human, etc.
 	}
 
 	getSprite(){
-		// switch(id){
-		// 	case 01:
-  //           {
+		switch(id){
+			case 1:
+            {
                
-  //           }
-		// 	break;
-		// 	case 02:
-  //           {
+            }
+			break;
+			case 2:
+            {
 
-  //           }
-		// 	break;
-		// 	case 03:
-  //           {
+            }
+			break;
+			case 3:
+            {
 
-  //           }
-		// 	break;
-		// 	case 04:
-  //           {
+            }
+			break;
+			case 4:
+            {
 
-  //           }
-		// 	break;
-		// 	case 05:
-  //           {
+            }
+			break;
+			case 5:
+            {
 
-  //           }
-		// 	break;
-  //           case 06:
-  //           {
+            }
+			break;
+            case 6:
+            {
 
-  //           }
-		// 	break;
-		// }
+            }
+			break;
+		}
 	}
 
 	calculatingPay(){
        // TODO calculating the accumulated pay amount 
+       switch(emotion){
+       	case "unhappy": payAmount += 0;
+       	break;
+       	case "netural": payAmount += 1;
+       	break;
+       	case "happy":   payAmount += 2;
+       	break;
+       }
 	}
 
 	actualPay(){
-       // TODO pay the rent
+		var temp = payAmount;
+		payAmount = 0;
+       return temp;
 	}
 
-	get Strength(){
-      // TODO get wifi strength\
-      calculateEmotion();
-      return this.strength;
+	getStrength({row = 0, col =0} = {}){
 
+    
 	}
 
 	calculateEmotion(){
+		if(strength < low) emotion = "unhappy"
+			else if(strength < midlow) emotion = "netural"
+				else if(strength < midhigh) emotion = "happy"
+					else if(strength < high) emotion = "netural"
+						else emotion = "unhappy"
+
+	    calculatingPay();
      // TODO calculate emotion based on wifi strength
 	}
 
